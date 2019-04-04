d2ff6b81b7c281d82d8b14488195177286045f2b
ralphkrauss <33028950+ralphkrauss@users.noreply.github.com>
2019-04-04 21:59:55 +0200

    fotos voor verslag + t changes

--- Splines/approx.m
+++ Splines/approx.m
@@ -16,10 +16,18 @@ function approx()
     
     % TODO: x veranderen van de index naar de afstand tussen de
     % opeenvolgende punten
-    x = 0:1:n;
+%     x = 0:1:n;
+    x = zeros(1, n+1);
+    x(1) = 0;
+    sum = 0;
+    for i=1:n-1
+        dist = sqrt( power( (A(1, i+1) - (A(1, i))) , 2) + power( (A(2, i+1) - (A(2, i))) , 2) );
+        x(i+1) = sum + dist;
+        sum = sum + dist;
+    end
+    x(n+1) = sum + sqrt( power( (A(1, 1) - (A(1, n))) , 2) + power( (A(2, 1) - (A(2, n))) , 2) );
     y = A;
-    t = (0:n/(aantal_punten_per_spline*aantal_punten-1):n);
-    
+    t = (0:x(n+1)/(aantal_punten_per_spline*aantal_punten-1):x(n+1));    
     
     Y = periospline(x, y, t);
     plot(Y(1, :), Y(2, :));
--- Splines/approxSin.m
+++ Splines/approxSin.m
@@ -1,11 +1,12 @@
 function approxSin()
 
-    aantal_punten = 10;
+    aantal_punten =10;
     aantal_punten_per_spline = 500;
     y = @(x)(sin(x) + sin(4*x)/2);
     
     % Part one: plot the function with the points indicated
-    x = 0:2*pi/(aantal_punten-1):2*pi;
+%     x = 0:2*pi/(aantal_punten-1):2*pi;
+    x = [0, 0.50 ,1.114, 1.917, 2.881, 3.406, 4.374, 5.104,5.779, 2*pi];
     fprintf('%s points.\n',num2str(length(x)));
     f = y(x);
     figure;
@@ -20,6 +21,8 @@ function approxSin()
     Y = periospline(x, f, t);
     hold on
     plot(t, Y);
+    hold on
+    plot(t, y(t));
     title("sin(x) + sin(4*x)/2 with " + aantal_punten + " points")
     xlabel("x");
     ylabel("y");
@@ -34,24 +37,24 @@ function approxSin()
     ylabel("error");
     hold off;
     
-    % Part three: Plot the maximum error in function of number of points
-    figure;
-    t = (0:2*pi/1000:2*pi);
-    eval = y(t);
-    xxx = 3:1:200;
-    yyy = zeros(98, 1);
-    for n=3:200
-        x = 0:2*pi/(n-1):2*pi;
-        f = y(x);
-        f = f(1:end-1);
-        Y = periospline(x, f, t);
-        yyy(n-2) = max(Y-eval);
-    end
-    semilogy(xxx, yyy);
-    
-    xlabel("number of points");
-    ylabel("max error");
-    title("Max error in function of number of points");
-    hold off;
+%     % Part three: Plot the maximum error in function of number of points
+%     figure;
+%     t = (0:2*pi/1000:2*pi);
+%     eval = y(t);
+%     xxx = 3:1:5000;
+%     yyy = zeros(98, 1);
+%     for n=3:5000
+%         x = 0:2*pi/(n-1):2*pi;
+%         f = y(x);
+%         f = f(1:end-1);
+%         Y = periospline(x, f, t);
+%         yyy(n-2) = max(Y-eval);
+%     end
+%     semilogy(xxx, yyy);
+%     
+%     xlabel("number of points");
+%     ylabel("max error");
+%     title("Max error in function of number of points");
+%     hold off;
     
 end
